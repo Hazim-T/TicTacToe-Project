79d46a6268d493a0691b5d501302cae7349d5bd1
Hazim Al-Abdalla <hazimabd2@gmail.com>
2024-05-23 23:54:50 +0200

    fix: made player names a required field

--- src/main/java/boardgame/ui/GameStartController.java
+++ src/main/java/boardgame/ui/GameStartController.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import org.tinylog.Logger;
@@ -20,15 +21,22 @@ public class GameStartController {
     private TextField player2NameField;
 
     @FXML
+    private Label errorLabel;
+
+    @FXML
     private void handleNextButton(ActionEvent event) throws IOException {
-        Logger.debug("Player 1 name entered: {}", player1NameField.getText());
-        Logger.debug("Player 2 name entered: {}", player2NameField.getText());
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/ui.fxml"));
-        Parent root = fxmlLoader.load();
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        Players players = new Players(player1NameField.getText(), player2NameField.getText());
-        stage.setUserData(players);
-        stage.setScene(new Scene(root));
-        stage.show();
+        if (player1NameField.getText().isBlank() || player2NameField.getText().isBlank()) {
+            errorLabel.setText("Please enter your name!");
+        } else {
+            Logger.debug("Player 1 name entered: {}", player1NameField.getText());
+            Logger.debug("Player 2 name entered: {}", player2NameField.getText());
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/ui.fxml"));
+            Parent root = fxmlLoader.load();
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            Players players = new Players(player1NameField.getText(), player2NameField.getText());
+            stage.setUserData(players);
+            stage.setScene(new Scene(root));
+            stage.show();
+        }
     }
 }
